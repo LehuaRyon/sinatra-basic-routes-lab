39a61978b1fd38ba8cddbc215f822a2b522d666e
Jasmine Ryon <jryon12@priorypride.net>
2021-04-22 09:19:54 -0700

    commit tree b122a5b64e4ce9b53581e264a259a8f6dd73d53d

--- app.rb
+++ app.rb
@@ -1,4 +1,34 @@
 require_relative 'config/environment'
 
+# class App < Sinatra::Base
+
+#     get '/name' do
+#         "My name is __"
+#     end
+
+#     get '/hometown' do
+#         "My hometown is __"
+#     end
+
+#     get '/favorite-song' do
+#         "My favorite song is __"
+#     end
+
+# end
+
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Jasmine"
+    end
+
+    get '/hometown' do
+        "My hometown is Honolulu"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Slow Dancing In A Burning Room"
+    end
+
 end
+
